5709455e1c085ef7071cfffb682e1ec79f82c82e
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-05-06 22:53:19 +0200

    added null handling

--- app/models/tags.rb
+++ app/models/tags.rb
@@ -1,7 +1,12 @@
+class NullTag
+	attr_reader :guid
+end
+
 class Tags
 	include EvernoteHelpers
 
 	def initialize
+		p 'initialize'
 		@session = EvernoteSession.sharedSession
 		@note_store = EvernoteNoteStore.noteStore
 		@note_store.listTagsWithSuccess tags_loaded, failure: output_error
@@ -9,10 +14,10 @@ class Tags
 
 	def tags_loaded
 		lambda do |tags|
-			where_tag = tags.select {|t| t.name == 'Where'}[0]
-			when_tag = tags.select {|t| t.name == 'When'}[0]
-			what_tag = tags.select {|t| t.name == 'What'}[0]
-			who_tag = tags.select {|t| t.name == 'Who'}[0]
+			where_tag = tags.select {|t| t.name == 'Where'}[0] || NullTag.new
+			when_tag = tags.select {|t| t.name == 'When'}[0] || NullTag.new
+			what_tag = tags.select {|t| t.name == 'What'}[0] || NullTag.new
+			who_tag = tags.select {|t| t.name == 'Who'}[0] || NullTag.new
 			
 			@where_tags = tags.select {|t| t.parentGuid == where_tag.guid }
 			@when_tags = tags.select {|t| t.parentGuid == when_tag.guid }
@@ -20,14 +25,15 @@ class Tags
 			@who_tags = tags.select {|t| t.parentGuid == who_tag.guid }
 
 			@when_tags.each do |tag|
-				p tag.name
-				p tag.parentGuid
+				p tag
 			end
 		end
 	end
 
 	def when
 		['!-Daily', '1-Now', '2-Next', '3-Soon']
+		#p @when_tags.length
+		#@when_tags.map {|tag| tag.name}
 	end
 
 	def what 
