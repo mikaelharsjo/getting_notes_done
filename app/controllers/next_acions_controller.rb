b4eee17b65b8567c163fdce491c9e2b255cb325d
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-05-27 23:34:08 +0200

    added basic filtering

--- app/controllers/next_acions_controller.rb
+++ app/controllers/next_acions_controller.rb
@@ -41,7 +41,7 @@ class NextActionsController < UITableViewController
 		self.refreshControl.beginRefreshing
 		@session = EvernoteSession.sharedSession
 		@note_store = EvernoteNoteStore.noteStore
-		filter = EDAMNoteFilter.alloc.initWithOrder 0, ascending:false, words:nil, notebookGuid:nil, tagGuids:nil, timeZone:nil, inactive:false, emphasized:nil
+		filter = EDAMNoteFilter.alloc.initWithOrder 0, ascending:false, words:nil, notebookGuid:nil, tagGuids: @filter.tag_guids, timeZone:nil, inactive:false, emphasized:nil
 
 		spec = EDAMNotesMetadataResultSpec.alloc.initWithIncludeTitle true, includeContentLength:false, includeCreated:false, includeUpdated:false, includeUpdateSequenceNum:false, includeNotebookGuid:false, includeTagGuids:true, includeAttributes:false, includeLargestResourceMime:false, includeLargestResourceSize:false
 		@note_store.findNotesMetadataWithFilter filter, offset:0, maxNotes:10, resultSpec:spec, success: notes_loaded, failure: output_error
--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,6 +1,12 @@
 class Filter # not needed?
 	
 	def initialize tags
-		#@tags = tags
+		@tags = tags
+	end
+
+	def tag_guids
+		p @tags.where_guids[0]
+		array = Array.new
+		array << @tags.where_guids.first
 	end
 end
