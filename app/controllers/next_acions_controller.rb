0e44ace1f194a39291722276285fb0287cd73a28
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-06-02 21:17:41 +0200

    wrong fetch method corrected

--- app/controllers/next_acions_controller.rb
+++ app/controllers/next_acions_controller.rb
@@ -26,25 +26,25 @@ class NextActionsController < UITableViewController
 		self.tableView.backgroundView = image_view
 
 		self.refreshControl = UIRefreshControl.alloc.init
-		self.refreshControl.addTarget self, action: 'load_actions_from_evernote', forControlEvents: UIControlEventValueChanged
+		self.refreshControl.addTarget self, action: 'fetch_actions_from_evernote', forControlEvents: UIControlEventValueChanged
 
 		nav_add_button = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAdd, target: self, action: 'add_action')
 		self.navigationItem.rightBarButtonItem = nav_add_button		
 	end
 
-	def viewDidAppear(animated)
-		@filter = Filter.new @tags
+	def viewDidAppear(animated)		
 		fetch_actions_from_evernote
 	end
 
 	def fetch_actions_from_evernote
+		filter = Filter.new @tags
 		self.refreshControl.beginRefreshing
-		@session = EvernoteSession.sharedSession
-		@note_store = EvernoteNoteStore.noteStore
-		filter = EDAMNoteFilter.alloc.initWithOrder 0, ascending:false, words:nil, notebookGuid:nil, tagGuids: @filter.tag_guids, timeZone:nil, inactive:false, emphasized:nil
+		session = EvernoteSession.sharedSession
+		note_store = EvernoteNoteStore.noteStore
+		filter = EDAMNoteFilter.alloc.initWithOrder 0, ascending:false, words:nil, notebookGuid:nil, tagGuids: filter.tag_guids, timeZone:nil, inactive:false, emphasized:nil
 
 		spec = EDAMNotesMetadataResultSpec.alloc.initWithIncludeTitle true, includeContentLength:false, includeCreated:false, includeUpdated:false, includeUpdateSequenceNum:false, includeNotebookGuid:false, includeTagGuids:true, includeAttributes:false, includeLargestResourceMime:false, includeLargestResourceSize:false
-		@note_store.findNotesMetadataWithFilter filter, offset:0, maxNotes:10, resultSpec:spec, success: notes_loaded, failure: output_error
+		note_store.findNotesMetadataWithFilter filter, offset:0, maxNotes:10, resultSpec:spec, success: notes_loaded, failure: output_error
 	end
 
 	def notes_loaded
