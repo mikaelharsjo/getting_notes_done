884ec7d50b53523ab275535409fe21ddae456a84
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-06-02 22:06:10 +0200

    filter search

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -2,11 +2,23 @@ class Filter # not needed?
 	
 	def initialize tags
 		@tags = tags
+		@context = Context.new
 	end
 
 	def tag_guids
-		p @tags.where_guids[0]
 		array = Array.new
-		array << @tags.where_guids.first
+		array << @tags.where_tags.select {|tag| tag.name == @context.where}[0].guid
+		array << @tags.what_tags.select {|tag| tag.name == @context.what}[0].guid
+		array << @tags.when_tags.select {|tag| tag.name == @context.when}[0].guid
+		#if @context.what
+		#	p @context.what
+		#end
+		#if @context.when
+		#	p @context.when
+		#end
+
+		#array << @tags.where_guids.first
+		p array
+		array
 	end
 end
