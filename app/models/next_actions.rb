132dc2b1a745e1bc67e41c283c66934b871ec874
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-07-26 13:56:12 +0200

    codes styling

--- app/models/next_actions.rb
+++ app/models/next_actions.rb
@@ -1,25 +1,22 @@
 class NextActionsFetcher
-	def self.fetch filter, &block
-		#self.refreshControl.beginRefreshing
+	# why is filter a param?
+	def self.fetch(filter, &block)
 		@session = EvernoteSession.sharedSession
 		@note_store = EvernoteNoteStore.noteStore
-		filter = EDAMNoteFilter.alloc.initWithOrder 0, ascending:false, words:nil, notebookGuid:nil, tagGuids: filter.tag_guids, timeZone:nil, inactive:false, emphasized:nil
+		filter = EDAMNoteFilter.alloc.initWithOrder 0, ascending: false, words: nil, notebookGuid: nil, tagGuids: filter.tag_guids, timeZone: nil, inactive: false, emphasized: nil
 
-		spec = EDAMNotesMetadataResultSpec.alloc.initWithIncludeTitle true, includeContentLength:false, includeCreated:false, includeUpdated:false, includeUpdateSequenceNum:false, includeNotebookGuid:false, includeTagGuids:true, includeAttributes:false, includeLargestResourceMime:false, includeLargestResourceSize:false
-		@note_store.findNotesMetadataWithFilter filter, offset:0, maxNotes:10, resultSpec:spec, success: notes_loaded, failure: output_error
+		spec = EDAMNotesMetadataResultSpec.alloc.initWithIncludeTitle true, includeContentLength: false, includeCreated: false, includeUpdated: false, includeUpdateSequenceNum: false, includeNotebookGuid: false, includeTagGuids: true, includeAttributes: false, includeLargestResourceMime: false, includeLargestResourceSize: false
+		@note_store.findNotesMetadataWithFilter filter, offset: 0, maxNotes: 10, resultSpec: spec, success: notes_loaded, failure: nil
 
 		block.call
 	end
 
-	def self.notes_loaded &block
+	def self.notes_loaded(&block)
 		@notes.clear
 		lambda do |meta_data|
-			meta_data.notes.each do |note| 
+			meta_data.notes.each do |note|
 				@notes << Note.new(note.title)
 			end
-
-			#self.refreshControl.endRefreshing
-			#self.tableView.reloadData
 		end
 	end
 end
