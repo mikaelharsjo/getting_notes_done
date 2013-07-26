c563e3dd740a460cd5e781b8f0933ce0518d006e
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-07-26 21:17:09 +0200

    code styling

--- app/models/action_fetcher.rb
+++ app/models/action_fetcher.rb
@@ -1,15 +1,46 @@
 class ActionFetcher
-	include EvernoteHelpers
-
-	def initialize filter
+	def initialize(filter)
 		@filter = filter
-		session = EvernoteSession.sharedSession
-		@note_store = EvernoteNoteStore.noteStore		
+		@note_store = EvernoteNoteStore.noteStore
 	end
 
-	def fetch success
-		filter = EDAMNoteFilter.alloc.initWithOrder 0, ascending:false, words:nil, notebookGuid: @filter.notebook_guid, tagGuids: @filter.tag_guids, timeZone:nil, inactive:false, emphasized:nil
-		spec = EDAMNotesMetadataResultSpec.alloc.initWithIncludeTitle true, includeContentLength:false, includeCreated:false, includeUpdated:false, includeUpdateSequenceNum:false, includeNotebookGuid:false, includeTagGuids:true, includeAttributes:false, includeLargestResourceMime:false, includeLargestResourceSize:false
-		@note_store.findNotesMetadataWithFilter filter, offset:0, maxNotes:10, resultSpec: spec, success: success, failure: output_error		
+	def fetch(success)
+		filter = create_filter
+		spec = create_spec
+		@note_store.findNotesMetadataWithFilter(
+			filter,
+			offset: 0,
+			maxNotes: 10,
+			resultSpec: spec,
+			success: success,
+			failure: nil)
+	end
+
+	private
+
+	def create_filter
+		EDAMNoteFilter.alloc.initWithOrder(
+			0,
+			ascending: false,
+			words: nil,
+			notebookGuid: @filter.notebook_guid,
+			tagGuids: @filter.tag_guids,
+			timeZone: nil,
+			inactive: false,
+			emphasized: nil)
+	end
+
+	def create_spec
+		EDAMNotesMetadataResultSpec.alloc.initWithIncludeTitle(
+			true,
+			includeContentLength: false,
+			includeCreated: false,
+			includeUpdated: false,
+			includeUpdateSequenceNum: false,
+			includeNotebookGuid: false,
+			includeTagGuids: true,
+			includeAttributes: false,
+			includeLargestResourceMime: false,
+			includeLargestResourceSize: false)
 	end
 end
--- app/models/action_presenter.rb
+++ app/models/action_presenter.rb
@@ -3,19 +3,19 @@ class ActionPresenter
 		lambda do |meta_data|
 			actions = Array.new
 			# remove nils
-			meta_data.notes.map {|note| note.tagGuids = note.tagGuids.to_a}
+			meta_data.notes.map { |note| note.tagGuids = note.tagGuids.to_a }
 
 			meta_data.notes.each do |note|
 				when_tags = []
-				note.tagGuids.each do |tag_guid| 
-					when_tags = tags.when_tags.select {|when_tag| when_tag.guid == tag_guid}
+				note.tagGuids.each do |tag_guid|
+					when_tags = tags.when_tags.select { |when_tag| when_tag.guid == tag_guid }
 				end
-				
-				actions << Note.new(note.title, note.guid, when_tags.first)				
+
+				actions << Note.new(note.title, note.guid, when_tags.first)
 			end
 
-			actions.sort! {|a, b| a.when.name <=> b.when.name}
-			
+			actions.sort! { |a, b| a.when.name <=> b.when.name }
+
 			block.call(actions)
 		end
 	end
